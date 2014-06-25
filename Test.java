577b6666b53d54b997a1eebd6be6288771d19ba8
Presish Chand <s_chand@u.pacific.edu>
2014-06-25 00:10:00 -0700

    JavaTest
    my practice in java

---
+++ Test.java
@@ -0,0 +1,12 @@
+class Test{
+
+	public static void main(String[] args){
+		
+		char[] myArray = new char[10];
+		char letter = 'a';
+		for(int i = 0; i< myArray.length;i++){
+			myArray[i] = letter;
+			System.out.println(myArray[i]);
+		}
+	}
+}
